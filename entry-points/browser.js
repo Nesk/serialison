2bf33fb488eb0ea46f3bc5d8ba21d45bfae9909a
Johann Pardanaud <pardanaud.j@gmail.com>
2015-03-22 17:01:19 +0100

    Reduce the size of the client file
    Done by requiring only what the project needs in core-js

--- entry-points/browser.js
+++ entry-points/browser.js
@@ -1,5 +1,9 @@
-// We need the polyfill
-require('babel/polyfill');
+// We want only what we need to reduce the size of the final output
+require('core-js/src/es6.symbol');
+require('core-js/src/es6.object.statics');
+require('core-js/src/es6.array.statics');
+require('core-js/src/es6.collections');
+require('regenerator-babel/runtime');
 
 // Export the library
 module.exports = require('../lib/serialison');
